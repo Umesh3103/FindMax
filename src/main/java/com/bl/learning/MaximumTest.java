1745053b17cc06c1014f764c8bbc1e3802fc774d
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-09 12:13:33 +0530

    [Umesh]Added: finding maximum integer

--- src/main/java/com/bl/learning/MaximumTest.java
+++ src/main/java/com/bl/learning/MaximumTest.java
@@ -4,4 +4,14 @@ public class MaximumTest {
 	public static void main(String[] args) {
 		System.out.println("welcome to find max problem");
 	}
+	public String findMax(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		return ""+max;
+	}
 }
