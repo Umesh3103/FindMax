32e05235c4e23afe0477406b2f7c6591080d78af
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-09 13:21:39 +0530

    [Umesh]Added: finding maximum for more than 3 inputs

--- src/main/java/com/bl/learning/MaximumTest.java
+++ src/main/java/com/bl/learning/MaximumTest.java
@@ -6,17 +6,19 @@ public class MaximumTest<E extends Comparable<E>> {
 	}
 	
 	E x, y, z;
+	E[] elements;
 	
-	public MaximumTest(E x, E y, E z) {
+	public MaximumTest(E x, E y, E z, E... elements) {
 		super();
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.elements=elements;
 	}
 	public String findMax(){
-		return findMax(x,y,z);
+		return findMax(x,y,z,elements);
 	}
-	public static <E extends Comparable<E>> String findMax(E x, E y, E z){
+	public static <E extends Comparable<E>> String findMax(E x, E y, E z, E... elements){
 		E max = x;
 		if (y.compareTo(max) > 0) {
 			max = y;
@@ -24,6 +26,17 @@ public class MaximumTest<E extends Comparable<E>> {
 		if (z.compareTo(max) > 0) {
 			max = z;
 		}
+		if(elements.length!=0){
+			max=findOptional(max,elements);
+		}
 		return ""+max;
+	}
+	private static <E extends Comparable<E>> E findOptional(E max, E[] elements) {
+		for(E entry: elements){
+			if(entry.compareTo(max)>0){
+				max=entry;
+			}
+		}
+		return max;
 	}
 }
--- src/test/java/com/bl/learning/MaxTest.java
+++ src/test/java/com/bl/learning/MaxTest.java
@@ -51,4 +51,19 @@ public class MaxTest {
 		String result = new MaximumTest("Apple","Banana","Peach").findMax();
 		Assert.assertEquals("Peach", result);
 	}
+	@Test
+	public void GivenManyIntegers_UsingGenerics_ShouldReturnTrue(){
+		String result = new MaximumTest(3,2,1,5,10).findMax();
+		Assert.assertEquals("10", result);
+	}
+	@Test
+	public void GivenManyFloats_UsingGenerics_ShouldReturnTrue(){
+		String result = new MaximumTest(3.0f,2.0f,1.0f,4.0f,7.0f,9.0f).findMax();
+		Assert.assertEquals("9.0", result);
+	}
+	@Test
+	public void GivenManyStrings_UsingGenerics_ShouldReturnTrue(){
+		String result = new MaximumTest("Apple","Peach","Banana","Zebra","Elephant").findMax();
+		Assert.assertEquals("Zebra", result);
+	}
 }
