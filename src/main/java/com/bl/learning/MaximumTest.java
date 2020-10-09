bf53425fd77819214c630fb2d73057b48d2f49c4
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-09 13:12:24 +0530

    [Umesh]Added: finding max using generics class

--- src/main/java/com/bl/learning/MaximumTest.java
+++ src/main/java/com/bl/learning/MaximumTest.java
@@ -1,10 +1,21 @@
 package com.bl.learning;
 
-public class MaximumTest {
+public class MaximumTest<E extends Comparable<E>> {
 	public static void main(String[] args) {
 		System.out.println("welcome to find max problem");
 	}
 	
+	E x, y, z;
+	
+	public MaximumTest(E x, E y, E z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public String findMax(){
+		return findMax(x,y,z);
+	}
 	public static <E extends Comparable<E>> String findMax(E x, E y, E z){
 		E max = x;
 		if (y.compareTo(max) > 0) {
--- src/test/java/com/bl/learning/MaxTest.java
+++ src/test/java/com/bl/learning/MaxTest.java
@@ -8,47 +8,47 @@ public class MaxTest {
 	
 	@Test
 	public void GivenIntegerFirstPosition_UsingGenerics_ShouldReturnTrue(){
-		String result = MaximumTest.findMax(3,2,1);
+		String result = new MaximumTest(3,2,1).findMax();
 		Assert.assertEquals("3", result);
 	}
 	@Test
 	public void GivenIntegerSecondPosition_UsingGenerics_ShouldReturnTrue(){
-		String result = MaximumTest.findMax(2,3,1);
+		String result = new MaximumTest(2,3,1).findMax();
 		Assert.assertEquals("3", result);
 	}
 	@Test
 	public void GivenIntegerThirdPosition_UsingGenerics_ShouldReturnTrue(){
-		String result = MaximumTest.findMax(1,2,3);
+		String result =new MaximumTest(1,2,3).findMax();
 		Assert.assertEquals("3", result);
 	}
 	@Test
 	public void GivenFloatFirstPosition_UsingGenerics_ShouldReturnTrue(){
-		String result = MaximumTest.findMax(3.0f,2.0f,1.0f);
+		String result = new MaximumTest(3.0f,2.0f,1.0f).findMax();
 		Assert.assertEquals("3.0", result);
 	}
 	@Test
 	public void GivenFloatSecondPosition_UsingGenerics_ShouldReturnTrue(){
-		String result = MaximumTest.findMax(2.0f,3.0f,1.0f);
+		String result = new MaximumTest(2.0f,3.0f,1.0f).findMax();
 		Assert.assertEquals("3.0", result);
 	}
 	@Test
 	public void GivenFloatThirdPosition_UsingGenerics_ShouldReturnTrue(){
-		String result = MaximumTest.findMax(1.0f,2.0f,3.0f);
+		String result = new MaximumTest(1.0f,2.0f,3.0f).findMax();
 		Assert.assertEquals("3.0", result);
 	}
 	@Test
 	public void GivenStringFirstPosition_UsingGenerics_ShouldReturnTrue(){
-		String result = MaximumTest.findMax("Peach","Apple","Banana");
+		String result =new MaximumTest("Peach","Apple","Banana").findMax();
 		Assert.assertEquals("Peach", result);
 	}
 	@Test
 	public void GivenStringSecondPosition_UsingGenerics_ShouldReturnTrue(){
-		String result = MaximumTest.findMax("Apple","Peach","Banana");
+		String result = new MaximumTest("Apple","Peach","Banana").findMax();
 		Assert.assertEquals("Peach", result);
 	}
 	@Test
 	public void GivenStringThirdPosition_UsingGenerics_ShouldReturnTrue(){
-		String result = MaximumTest.findMax("Banana","Apple","Peach");
+		String result = new MaximumTest("Apple","Banana","Peach").findMax();
 		Assert.assertEquals("Peach", result);
 	}
 }
