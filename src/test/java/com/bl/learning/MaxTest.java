d5718445900131f8507dcb25b404f4d04e7b054d
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-09 12:21:49 +0530

    [Umesh]Added: finding maximum integer when it is at second position

--- src/test/java/com/bl/learning/MaxTest.java
+++ src/test/java/com/bl/learning/MaxTest.java
@@ -11,5 +11,10 @@ public class MaxTest {
 		String result = MaximumTest.findMax(3, 2, 1);
 		Assert.assertEquals("3", result);
 	}
+	@Test
+	public void GivenInteger_AtSecondPosition_ShouldReturnTrue(){
+		String result = MaximumTest.findMax(2, 3, 1);
+		Assert.assertEquals("3", result);
+	}
 	
 }
